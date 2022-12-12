e70f04cc4352021c6a356e0a3f505706bf5a4f08
Federico Barrientos <fgbarrientos93@gmail.com>
2022-12-12 23:40:40 +0100

    Added MUI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button  from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='container-careta'>
+          <TextField id="standard-basic" label="Anda pa'lla" variant="standard"/>
+          <Button variant="contained">Bobo</Button>
+        </div>
       </header>
     </div>
   );
