130fdcbf3d31bcbf3b9885addc80c332c1b844e6
Federico Barrientos <fgbarrientos93@gmail.com>
2022-12-27 19:48:12 +0100

    Added css file for signin

--- src/containers/signin/SignIn.jsx
+++ src/containers/signin/SignIn.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import './signin.css';
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div>
+      <h3>SignIn</h3>
+      <p>hola soy el signin</p>
+    </div>
   )
 }
